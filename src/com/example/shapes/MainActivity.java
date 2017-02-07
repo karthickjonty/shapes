53fb726d96314a11e729421eb952a45f781fcaed
ITLAB <ITLAB@IT>
2017-02-07 14:44:25 +0530

    second commit

--- src/com/example/shapes/MainActivity.java
+++ src/com/example/shapes/MainActivity.java
@@ -1,85 +1,70 @@
-import java.util.ArrayList;
-
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.LinearLayout;
-
-public class DrwaingActivity extends Activity {
-
- View mView; 
- private Paint mPaint;
- 
- @Override
- protected void onCreate(Bundle savedInstanceState) {
-  super.onCreate(savedInstanceState);
-  setContentView(R.layout.activity_main);
-  LinearLayout layout = (LinearLayout) findViewById(R.id.myDrawing);
-  mView = new DrawingView(this);
-  layout.addView(mView, new LayoutParams(
-    LinearLayout.LayoutParams.MATCH_PARENT,
-    LinearLayout.LayoutParams.MATCH_PARENT));
-  init(); 
- }
-
- private void init() {
-  mPaint = new Paint();
-  mPaint.setDither(true);
-  mPaint.setColor(0xFFFFFF00);
-  mPaint.setStyle(Paint.Style.STROKE);
-  mPaint.setStrokeJoin(Paint.Join.ROUND);
-  mPaint.setStrokeCap(Paint.Cap.ROUND);
-  mPaint.setStrokeWidth(3);
- }
-
- class DrawingView extends View {
-  private Path path;
-  private Bitmap mBitmap;
-  private Canvas mCanvas;
-
-  public DrawingView(Context context) {
-   super(context);
-   path = new Path();
-   mBitmap = Bitmap.createBitmap(820, 480, Bitmap.Config.ARGB_8888);
-   mCanvas = new Canvas(mBitmap);
-   this.setBackgroundColor(Color.BLACK);
-  }
-
-  private ArrayList<PathWithPaint> _graphics1 = new ArrayList<PathWithPaint>();
-
-  @Override
-  public boolean onTouchEvent(MotionEvent event) {
-   PathWithPaint pp = new PathWithPaint();
-   mCanvas.drawPath(path, mPaint);
-   if (event.getAction() == MotionEvent.ACTION_DOWN) {
-    path.moveTo(event.getX(), event.getY());
-    path.lineTo(event.getX(), event.getY());
-   } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-    path.lineTo(event.getX(), event.getY());
-    pp.setPath(path);
-    pp.setmPaint(mPaint);
-    _graphics1.add(pp);
-   }
-   invalidate();
-   return true;
-  }
-
-  @Override
-  protected void onDraw(Canvas canvas) {
-   super.onDraw(canvas);
-   if (_graphics1.size() > 0) {
-    canvas.drawPath(
-      _graphics1.get(_graphics1.size() - 1).getPath(),
-      _graphics1.get(_graphics1.size() - 1).getmPaint());
-   }
-  }
- }
-}
+package com.example.simplegraphics;  
+  
+import android.os.Bundle;  
+import android.app.Activity;  
+import android.view.Menu;  
+import android.content.Context;  
+import android.graphics.Canvas;  
+import android.graphics.Color;  
+import android.graphics.Paint;  
+import android.view.View;  
+  
+public class MainActivity extends Activity {  
+  
+    DemoView demoview;  
+    /** Called when the activity is first created. */  
+    @Override  
+    public void onCreate(Bundle savedInstanceState) {  
+        super.onCreate(savedInstanceState);  
+        demoview = new DemoView(this);  
+        setContentView(demoview);  
+    }  
+  
+    private class DemoView extends View{  
+        public DemoView(Context context){  
+            super(context);  
+        }  
+  
+        @Override protected void onDraw(Canvas canvas) {  
+            super.onDraw(canvas);  
+  
+            // custom drawing code here  
+            Paint paint = new Paint();  
+            paint.setStyle(Paint.Style.FILL);  
+  
+            // make the entire canvas white  
+            paint.setColor(Color.WHITE);  
+            canvas.drawPaint(paint);  
+              
+            // draw blue circle with anti aliasing turned off  
+            paint.setAntiAlias(false);  
+            paint.setColor(Color.BLUE);  
+            canvas.drawCircle(20, 20, 15, paint);  
+  
+            // draw green circle with anti aliasing turned on  
+            paint.setAntiAlias(true);  
+            paint.setColor(Color.GREEN);  
+            canvas.drawCircle(60, 20, 15, paint);  
+  
+            // draw red rectangle with anti aliasing turned off  
+            paint.setAntiAlias(false);  
+            paint.setColor(Color.RED);  
+            canvas.drawRect(100, 5, 200, 30, paint);  
+                           
+            // draw the rotated text  
+            canvas.rotate(-45);  
+                      
+            paint.setStyle(Paint.Style.FILL);  
+            canvas.drawText("Graphics Rotation", 40, 180, paint);  
+              
+            //undo the rotate  
+            canvas.restore();  
+        }  
+    }  
+    @Override  
+    public boolean onCreateOptionsMenu(Menu menu) {  
+        // Inflate the menu; this adds items to the action bar if it is present.  
+        getMenuInflater().inflate(R.menu.main, menu);  
+        return true;  
+    }  
+}  
